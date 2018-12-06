c75459474e4e73ca2db625c685ba7566cd3b434b
ElyesKhama <khamassi@etud.insa-toulouse.fr>
2018-12-06 23:09:57 +0100

    third graph

--- gunAnalysis.R
+++ gunAnalysis.R
@@ -1,60 +1,58 @@
 #Read the file
 getwd()
 dataSet = read.csv('gun-violence-data_01-2013_03-2018.csv',header = TRUE,',')
+dataSetStatePopulation = read.csv('populationStates', header = TRUE, ',')
 
 #Describe file
 str(dataSet)
 head(dataSet)
-
+head(dataSet$state)
 #If you need to install ggplot2:
 #install.packages("ggplot2")
 
 #Libraries:
 library(ggplot2)
 library(plyr)
+library(RColorBrewer)
 
-#Command to extract Month and Year from date yy-mm-dd
-dataSet$Month = format(as.Date(dataSet$date, format="%Y-%m-%d"),"%Y-%m")
+#Command to extract Mois and Year from date yy-mm-dd
+dataSet$Mois = format(as.Date(dataSet$date, format="%Y-%m-%d"),"%Y-%m")
 dataSet$Year = format(as.Date(dataSet$date, format="%Y-%m-%d"),"%Y")
+dataSet = dataSet[format(as.Date(dataSet$date, format="%Y-%m-%d"),"%Y") != "2013", ]
+str(dataSet)
 
-#add population ??
+################# FIRST GRAPH ################# 
+nbrInjuredByMois = ddply(dataSet, 'Mois', summarise, SommeInjured = sum(n_injured))
+nbrKilledByMois = ddply(dataSet, 'Mois', summarise, SommeKilled = sum(n_killed))
+nbrKilledInjured = merge(nbrInjuredByMois, nbrKilledByMois, by="Mois")
 
-#TODO: See if there's something simpler
-nbrInjuredByMonth = ddply(dataSet, 'Month', summarise, SommeInjured = sum(n_injured))
-nbrKilledByMonth = ddply(dataSet, 'Month', summarise, SommeKilled = sum(n_killed))
-test = merge(nbrInjuredByMonth, nbrKilledByMonth, by="Month")
+ggplot(nbrKilledInjured, aes(Mois, group=1)) + geom_line(aes(y=SommeInjured, colour="Blessés")) + geom_line(aes(y=SommeKilled, colour="Tués")) + ggtitle("Nombre de tués/bléssés depuis 2014") + labs(color="") + ylab("Somme") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-ggplot(test, aes(Month, group=1)) + geom_line(aes(y=SommeInjured, colour="Injured")) + geom_line(aes(y=SommeKilled, colour="Killed")) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+################# FIRST GRAPH ################# 
 
-#Total of every years
-nbrKilledByState = ddply(dataSet, 'state', summarise, Somme = sum(n_killed))
-nbrKilledByStateTop5 = arrange(nbrKilledByState, -Somme)[1:5,]
-nbrKilledByStateAndYear = ddply(dataSet, c('state', 'Year'), summarise, Somme = sum(n_killed))
-nbrKilledByStateAndYearTop5 = subset(nbrKilledByStateAndYear, nbrKilledByStateAndYear$state %in% nbrKilledByStateTop5$state)
+################# SECOND GRAPH ################# 
 
-ggplot(nbrKilledByStateAndYearTop5, aes(x=reorder(state, -Somme), y=Somme)) + geom_bar(stat="identity") + facet_wrap("Year", scales = "free") 
-
-### Something better exists ? ? ? ?
 nbrKilledByStateAndYear2 = ddply(dataSet, c('state', 'Year'), summarise, Somme = sum(n_killed))
 nbrKilledByStateAndYear2014 = subset(nbrKilledByStateAndYear2, nbrKilledByStateAndYear2$Year == '2014')
-nbrKilledByStateAndYear2014Top5 = arrange(nbrKilledByStateAndYear2014, -Somme)[1:5,]
+nbrKilledByStateAndYear2014Top5 = arrange(nbrKilledByStateAndYear2014, -Somme)[1:10,]
 nbrKilledByStateAndYear2015 = subset(nbrKilledByStateAndYear2, nbrKilledByStateAndYear2$Year == '2015')
-nbrKilledByStateAndYear2015Top5 = arrange(nbrKilledByStateAndYear2015, -Somme)[1:5,]
+nbrKilledByStateAndYear2015Top5 = arrange(nbrKilledByStateAndYear2015, -Somme)[1:10,]
 nbrKilledByStateAndYear2016 = subset(nbrKilledByStateAndYear2, nbrKilledByStateAndYear2$Year == '2016')
-nbrKilledByStateAndYear2016Top5 = arrange(nbrKilledByStateAndYear2016, -Somme)[1:5,]
+nbrKilledByStateAndYear2016Top5 = arrange(nbrKilledByStateAndYear2016, -Somme)[1:10,]
 nbrKilledByStateAndYear2017 = subset(nbrKilledByStateAndYear2, nbrKilledByStateAndYear2$Year == '2017')
-nbrKilledByStateAndYear2017Top5 = arrange(nbrKilledByStateAndYear2017, -Somme)[1:5,]
-
+nbrKilledByStateAndYear2017Top5 = arrange(nbrKilledByStateAndYear2017, -Somme)[1:10,]
 nbrKilledByStateAndYearFinalTop5 = rbind(nbrKilledByStateAndYear2014Top5, nbrKilledByStateAndYear2015Top5, nbrKilledByStateAndYear2016Top5, nbrKilledByStateAndYear2017Top5)
 
-library(RColorBrewer)
-ggplot(nbrKilledByStateAndYearFinalTop5, aes(x=reorder(state, -Somme), y=Somme, fill=Somme))+ geom_bar(stat="identity") + facet_wrap("Year", scales = "free") + scale_fill_gradient(low = "green", high = "red")
-#Age ? 
+ggplot(nbrKilledByStateAndYearFinalTop5, aes(x=reorder(state, -Somme), y=Somme, fill=Somme))+ geom_bar(stat="identity") + facet_wrap("Year", scales = "free") + scale_fill_gradient(low = "green", high = "red") + xlab("État") + ggtitle("Nombre de tués par état et par année") +ylab("Nombre de tués") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
+################# SECOND GRAPH ################# 
 
+################# THIRD GRAPH ################# 
 
+nbrKilledByStateAndYear = ddply(dataSet, c('state', 'Year'), summarise, Somme = sum(n_killed))
+nbrKilledByStateAndYearWPopulation = merge(nbrKilledByStateAndYear, dataSetStatePopulation, by="state")
+nbrKilledByStateAndYearWPopulation$ratio = nbrKilledByStateAndYearWPopulation$Somme / nbrKilledByStateAndYearWPopulation$population *100
 
-# ratio n_killed / n_injured
-# top 5 states where max killed --> bigger states (???)
-# top 5 weapons (???)
-# age and weapons in terms of age (???)
+ggplot(nbrKilledByStateAndYearWPopulation, aes(x=state,y=ratio)) + geom_boxplot(fill='#A4A4A4', color="darkred") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Répartition du ratio tués/population sur 2013-2018 par état") + xlab("État") + ylab("Ratio tués/population (%)")  
+
+################# THIRD GRAPH ################# 
