dd505b366d4dfc5ca4fdba68433d9c3b7d5065ae
ElyesKhama <khamassi@etud.insa-toulouse.fr>
2018-12-07 20:14:02 +0100

    update script with all charts'

--- gunAnalysis.R
+++ gunAnalysis.R
@@ -1,7 +1,7 @@
 #Read the file
 getwd()
 dataSet = read.csv('gun-violence-data_01-2013_03-2018.csv',header = TRUE,',')
-dataSetStatePopulation = read.csv('populationStates', header = TRUE, ',')
+dataSetStatePopulation = read.csv('populationStates.csv', header = TRUE, ',')
 
 #Describe file
 str(dataSet)
@@ -9,11 +9,14 @@ head(dataSet)
 head(dataSet$state)
 #If you need to install ggplot2:
 #install.packages("ggplot2")
+install.packages("maps")
 
 #Libraries:
 library(ggplot2)
 library(plyr)
 library(RColorBrewer)
+library(stringr)
+library(maps)
 
 #Command to extract Mois and Year from date yy-mm-dd
 dataSet$Mois = format(as.Date(dataSet$date, format="%Y-%m-%d"),"%Y-%m")
@@ -56,3 +59,50 @@ nbrKilledByStateAndYearWPopulation$ratio = nbrKilledByStateAndYearWPopulation$So
 ggplot(nbrKilledByStateAndYearWPopulation, aes(x=state,y=ratio)) + geom_boxplot(fill='#A4A4A4', color="darkred") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Répartition du ratio tués/population sur 2013-2018 par état") + xlab("État") + ylab("Ratio tués/population (%)")  
 
 ################# THIRD GRAPH ################# 
+
+############################################# Map ##########################################################
+#load us map data
+all_states <- map_data("state")
+#plot all states with ggplot
+dataSetPost<-dataSet[dataSet$latitude < 50,]
+dataSetPreFinal<-dataSetPost[dataSetPost$longitude> -140,]
+dataSetFinal<-dataSetPreFinal[dataSetPreFinal$longitude< -30,]
+p <- ggplot()+ geom_polygon( data=all_states, aes(x=long, y=lat, group = group),fill = "red", color = "blue" )
+p <- p + geom_point(data=data.frame(long = dataSetFinal$longitude,lat =dataSetFinal$latitude, stringsAsFactors = FALSE),aes(x=long, y=lat, size = 4),shape=".",size = 4, color="Yellow") 
+p
+################################################ Gun ######################################################
+Armes=""
+for(armUsed in dataSet$gun_type ){
+  elems <-unlist(str_split(armUsed,"\\|\\|"))
+  for (value in elems) {
+    if (value=="") {}
+    else {
+      arms<-unlist(str_split(value,"[0-9]+::"))
+      for(arm in arms)
+        if(arm=="" || arm=="0:Unknown"){}
+      else{
+        Armes=c(Armes,arm)
+      }
+    }
+  }
+}
+mostUsed=as.data.frame(sort(table(Armes),TRUE)[1:5])
+ggplot(data=mostUsed, aes(x=Armes, y=Freq, fill=Armes)) + geom_bar(stat="identity")+ xlab("Type d'arme ") + ylab("Nombre de fois utilisée")
+################################################## Age #####################################################
+Ages=""
+for(ageUsed in dataSet$participant_age ){
+  elems <-unlist(str_split(ageUsed,"\\|\\|"))
+  for (value in elems) {
+    if (value=="") {}
+    else {
+      ages<-unlist(str_split(value,"[0-9]+::"))
+      for(age in ages)
+        if(age=="" || age=="0:Unknown"|| age=="Unknown"){}
+      else{
+        Ages=c(Ages,age)
+      }
+    }
+  }
+}
+mostUsed=as.data.frame(sort(table(Ages),TRUE)[1:14])
+ggplot(data=mostUsed, aes(x=Ages, y=Freq, fill=Ages)) + geom_bar(stat="identity")+ xlab("Age ") + ylab("Nombre de fois ")
